6948de8392c723765a90366b88b903d79d532b3c
chinbino <shoptahrir40@gmail.com>
2025-11-21 17:02:20 +0330

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { ProductsModule } from './products/products.module';
+import { CurrencyService } from './currency/currency.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
-  providers: [],
+  providers: [CurrencyService],
 })
 export class AppModule {}
