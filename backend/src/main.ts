b1b293b3ed38a36da3648ad98adf96a05cbc59d5
chinbino <shoptahrir40@gmail.com>
2025-11-21 17:21:28 +0330

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log('🚀 B2B Backend is running on: http://localhost:3000');
+  
+  // استفاده از پورت Render یا پورت پیش‌فرض
+  const port = process.env.PORT || 3000;
+  
+  await app.listen(port);
+  console.log(`🚀 B2B Backend is running on: http://localhost:${port}`);
 }
 bootstrap();
