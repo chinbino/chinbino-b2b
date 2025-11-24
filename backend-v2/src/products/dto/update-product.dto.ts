f978c9c90b98dda1eecb93d9658d705a8d00c7e6
chinbino <shoptahrir40@gmail.com>
2025-11-24 16:30:07 +0330

    Update update-product.dto.ts

--- backend-v2/src/products/dto/update-product.dto.ts
+++ backend-v2/src/products/dto/update-product.dto.ts
@@ -1,4 +1,75 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateProductDto } from './create-product.dto';
+import { IsOptional, IsString, IsNumber, IsPositive, Min } from 'class-validator';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto {
+  @IsOptional()
+  @IsString()
+  titleFa?: string;
+
+  @IsOptional()
+  @IsString()
+  titleZh?: string;
+
+  @IsOptional()
+  @IsString()
+  titleEn?: string;
+
+  @IsOptional()
+  @IsString()
+  descriptionFa?: string;
+
+  @IsOptional()
+  @IsString()
+  descriptionZh?: string;
+
+  @IsOptional()
+  @IsString()
+  descriptionEn?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  basePriceCNY?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  basePriceIRR?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  basePriceUSD?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  stockUnit?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  @Min(1)
+  unitsPerCarton?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  @Min(1)
+  minOrderCartons?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  @Min(1)
+  minOrderUnits?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  cartonWeightKg?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  cartonVolumeM3?: number;
+}
