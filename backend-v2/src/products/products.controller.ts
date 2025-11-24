40d7d10f1378a452151097565c3644733e0863b8
chinbino <shoptahrir40@gmail.com>
2025-11-24 08:41:30 +0330

    Create products.controller.ts

---
+++ backend-v2/src/products/products.controller.ts
@@ -0,0 +1,44 @@
+import { Controller, Get, Post, Body, Param, UseGuards, Request, Put, Delete } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  create(@Body() createProductDto: CreateProductDto, @Request() req) {
+    return this.productsService.create(createProductDto, req.user);
+  }
+
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+  @Get('my-products')
+  @UseGuards(JwtAuthGuard)
+  findMyProducts(@Request() req) {
+    return this.productsService.findBySeller(req.user.id);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.productsService.findOne(id);
+  }
+
+  @Put(':id')
+  @UseGuards(JwtAuthGuard)
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto, @Request() req) {
+    return this.productsService.update(id, updateProductDto, req.user);
+  }
+
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  remove(@Param('id') id: string, @Request() req) {
+    return this.productsService.remove(id, req.user);
+  }
+}
