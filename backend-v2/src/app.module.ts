ca4f0c5225dc0025fe696f8b5d489129b0e2ea78
chinbino <shoptahrir40@gmail.com>
2025-11-28 09:20:00 +0330

    Update app.module.ts

--- backend-v2/src/app.module.ts
+++ backend-v2/src/app.module.ts
@@ -5,6 +5,7 @@ import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { OrdersModule } from './orders/orders.module';
+import { CmsModule } from './cms/cms.module'; // اضافه کردن این خط
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { OrdersModule } from './orders/orders.module';
     UsersModule,
     AuthModule,
     OrdersModule,
+    CmsModule, // اضافه کردن این خط
   ],
   controllers: [AppController],
 })
