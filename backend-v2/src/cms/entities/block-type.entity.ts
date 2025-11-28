a93c30269aaafc4595eeffeac2a56a50a5499b99
chinbino <shoptahrir40@gmail.com>
2025-11-28 08:58:54 +0330

    Create block-type.entity.ts

---
+++ backend-v2/src/cms/entities/block-type.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from 'typeorm';
+
+@Entity('block_types')
+export class BlockType {
+  @PrimaryColumn('varchar', { length: 50 })
+  id: string;
+
+  @Column('jsonb')
+  schema: any;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
+}
