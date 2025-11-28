38d09570665746b5e01d9e8d326424261f8fb4c3
chinbino <shoptahrir40@gmail.com>
2025-11-28 09:03:14 +0330

    Create content-revision.entity.ts

---
+++ backend-v2/src/cms/entities/content-revision.entity.ts
@@ -0,0 +1,33 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, ManyToOne, JoinColumn, Index } from 'typeorm';
+import { Content } from './content.entity';
+import { User } from '../../users/entities/user.entity';
+
+@Entity('content_revisions')
+@Index('IDX_content_revisions_content_id', ['content'])
+export class ContentRevision {
+  @PrimaryGeneratedColumn('bigint')
+  id: number;
+
+  @ManyToOne(() => Content, content => content.id, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'content_id' })
+  content: Content;
+
+  @Column('jsonb')
+  blocks: any[];
+
+  @Column('jsonb', { nullable: true })
+  seo: any;
+
+  @Column('jsonb', { nullable: true })
+  meta: {
+    note?: string;
+    status?: string;
+  };
+
+  @ManyToOne(() => User, { nullable: true })
+  @JoinColumn({ name: 'author_id' })
+  author: User;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
+}
