001ce0a75efb7649b1d1a7e782d07e5d04a0a93d
chinbino <shoptahrir40@gmail.com>
2025-11-29 20:35:38 +0330

    Update cms.module.ts

--- backend-v2/src/cms/cms.module.ts
+++ backend-v2/src/cms/cms.module.ts
@@ -4,18 +4,18 @@ import { Content } from './entities/content.entity';
 import { ContentRevision } from './entities/content-revision.entity';
 import { ContentService } from './services/content.service';
 import { BlockRendererService } from './services/block-renderer.service';
-import { CmsController } from './controllers/cms.controller';
+import { CmsController } from './controllers/cms.controller'; // اضافه شود
 import { RenderController } from './controllers/render.controller';
-import { PublicPagesController } from './controllers/public-pages.controller'; // اضافه شود
+import { PublicPagesController } from './controllers/public-pages.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Content, ContentRevision]),
   ],
   controllers: [
-    CmsController, 
+    CmsController, // اضافه شود
     RenderController, 
-    PublicPagesController // اضافه شود
+    PublicPagesController
   ],
   providers: [ContentService, BlockRendererService],
   exports: [ContentService, BlockRendererService],
