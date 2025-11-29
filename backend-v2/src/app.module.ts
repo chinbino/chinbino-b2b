0275885cfbf90416286066bfde862d844a3ffc26
chinbino <shoptahrir40@gmail.com>
2025-11-29 19:58:01 +0330

    Update app.module.ts

--- backend-v2/src/app.module.ts
+++ backend-v2/src/app.module.ts
@@ -7,6 +7,9 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { CmsModule } from './cms/cms.module';
 import { AdminModule } from './admin/admin.module';
+import { PublicPagesController } from './cms/controllers/public-pages.controller'; // اضافه شود
+import { ContentService } from './cms/services/content.service';
+import { BlockRendererService } from './cms/services/block-renderer.service';
 
 @Module({
   imports: [
@@ -23,7 +26,7 @@ import { AdminModule } from './admin/admin.module';
     CmsModule,
     AdminModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PublicPagesController], // اضافه شود
+  providers: [AppService, ContentService, BlockRendererService], // اضافه شود
 })
 export class AppModule {}
