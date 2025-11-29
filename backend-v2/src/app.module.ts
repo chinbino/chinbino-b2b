7ae4bda2a215357b55bfd572bfb14534ac937f34
chinbino <shoptahrir40@gmail.com>
2025-11-29 20:35:14 +0330

    Update app.module.ts

--- backend-v2/src/app.module.ts
+++ backend-v2/src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService } from './app.service'; // اضافه شود
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { CmsModule } from './cms/cms.module';
 import { AdminModule } from './admin/admin.module';
-import { PublicPagesController } from './cms/controllers/public-pages.controller'; // اضافه شود
-import { ContentService } from './cms/services/content.service';
-import { BlockRendererService } from './cms/services/block-renderer.service';
 
 @Module({
   imports: [
@@ -26,7 +23,7 @@ import { BlockRendererService } from './cms/services/block-renderer.service';
     CmsModule,
     AdminModule,
   ],
-  controllers: [AppController, PublicPagesController], // اضافه شود
-  providers: [AppService, ContentService, BlockRendererService], // اضافه شود
+  controllers: [AppController],
+  providers: [AppService], // اضافه شود
 })
 export class AppModule {}
