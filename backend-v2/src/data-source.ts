e05386d30af639c6ae31ae1e4b77fc7dfc746b7d
chinbino <shoptahrir40@gmail.com>
2025-12-01 15:42:58 +0330

    Create data-source.ts

---
+++ backend-v2/src/data-source.ts
@@ -0,0 +1,13 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export default new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
+  synchronize: false,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+});
