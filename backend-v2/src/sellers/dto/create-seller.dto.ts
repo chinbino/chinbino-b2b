146cabf05a4fa026b8270e1e3140403ebf82f0c6
chinbino <shoptahrir40@gmail.com>
2025-12-02 08:59:05 +0330

    Create create-seller.dto.ts

---
+++ backend-v2/src/sellers/dto/create-seller.dto.ts
@@ -0,0 +1,50 @@
+import { IsString, IsOptional, IsPhoneNumber, Length, Matches } from 'class-validator';
+
+export class CreateSellerDto {
+  @IsString()
+  @Length(2, 255)
+  nameZh: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(2, 255)
+  nameFa?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(2, 255)
+  companyName?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(2, 255)
+  contactPerson?: string;
+
+  @IsOptional()
+  @IsString()
+  @Matches(/^\+?[1-9]\d{1,14}$/)
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(3, 50)
+  wechatId?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(3, 50)
+  aliwangwangId?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(5, 255)
+  location?: string;
+
+  @IsOptional()
+  @IsString()
+  descriptionZh?: string;
+
+  @IsOptional()
+  @IsString()
+  descriptionFa?: string;
+}
