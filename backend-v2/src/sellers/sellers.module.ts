760d1fedeafa3dcfca1084f92e0951f4e04c4742
chinbino <shoptahrir40@gmail.com>
2025-12-02 09:00:23 +0330

    Update sellers.module.ts

--- backend-v2/src/sellers/sellers.module.ts
+++ backend-v2/src/sellers/sellers.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Seller } from './entities/seller.entity';
+import { SellersService } from './sellers.service';
+import { SellersController } from './sellers.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Seller])],
-  providers: [],
-  controllers: [],
+  controllers: [SellersController],
+  providers: [SellersService],
+  exports: [SellersService], // برای استفاده در ماژول‌های دیگر
 })
 export class SellersModule {}
