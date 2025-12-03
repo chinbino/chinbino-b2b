0d54f7cb40f08c674cdecc2dd498bb8ed45f2408
chinbino <shoptahrir40@gmail.com>
2025-12-03 21:07:32 +0330

    Update roles.decorator.ts

--- backend-v2/src/auth/decorators/roles.decorator.ts
+++ backend-v2/src/auth/decorators/roles.decorator.ts
@@ -1,3 +1,5 @@
+// backend-v2/src/auth/decorators/roles.decorator.ts
 import { SetMetadata } from '@nestjs/common';
 
-export const Roles = (...roles: string[]) => SetMetadata('roles', roles);
+export const ROLES_KEY = 'roles'; // ✅ این خط اضافه شد
+export const Roles = (...roles: string[]) => SetMetadata(ROLES_KEY, roles);
