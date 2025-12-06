dd0c2e67e96b0cd79d63d8dd22d9c8025b702deb
chinbino <shoptahrir40@gmail.com>
2025-12-06 16:30:02 +0330

    Update data-source.ts

--- backend-v2/src/database/data-source.ts
+++ backend-v2/src/database/data-source.ts
@@ -7,7 +7,7 @@ config();
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  synchronize: false,
+  synchronize: false, // ✅ همیشه false
   logging: process.env.NODE_ENV !== 'production',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
