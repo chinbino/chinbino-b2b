9ab3ea477768dff3e8e3592065e876517e223aab
chinbino <shoptahrir40@gmail.com>
2025-12-06 16:28:55 +0330

    Update app.module.ts

--- backend-v2/src/app.module.ts
+++ backend-v2/src/app.module.ts
@@ -10,7 +10,7 @@ import { CmsModule } from './cms/cms.module';
 import { AdminModule } from './admin/admin.module';
 import { SellersModule } from './sellers/sellers.module';
 import { TestModule } from './test/test.module';
-import { SuppliersModule } from './suppliers/suppliers.module';
+// import { SuppliersModule } from './suppliers/suppliers.module'; // ❌ کامنت شد
 import { dataSourceOptions } from './database/data-source';
 
 @Module({
@@ -23,7 +23,7 @@ import { dataSourceOptions } from './database/data-source';
     AdminModule,
     SellersModule,
     TestModule,
-    SuppliersModule,
+    // SuppliersModule, // ❌ کامنت شد
   ],
   controllers: [AppController],
   providers: [AppService],
