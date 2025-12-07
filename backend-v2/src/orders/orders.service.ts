83a5bc11b83e40a3faeea6ce1857dbc3bb8c73be
chinbino <shoptahrir40@gmail.com>
2025-12-07 21:58:24 +0330

    Update orders.service.ts

--- backend-v2/src/orders/orders.service.ts
+++ backend-v2/src/orders/orders.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Order } from './entities/order.entity';
+import { OrderItem } from './entities/order-item.entity';
 
 @Injectable()
 export class OrdersService {
+  constructor(
+    @InjectRepository(Order) private ordersRepo: Repository<Order>,
+    @InjectRepository(OrderItem) private itemsRepo: Repository<OrderItem>
+  ) {}
 
-  async createOrder(data: any) {
-    return {
-      message: 'Order created (mock)',
-      data,
-    };
+  async create(data: { buyerId: number; items: { productId: number; quantity: number; }[]; }): Promise<Order> {
+    const order = this.ordersRepo.create({ buyerId: data.buyerId });
+    order.items = data.items.map(item => this.itemsRepo.create(item));
+    return this.ordersRepo.save(order);
   }
 
-  async getOrderById(id: string) {
-    return {
-      id,
-      status: 'pending',
-    };
+  async findOne(id: number): Promise<Order> {
+    return this.ordersRepo.findOne({
+      where: { id },
+      relations: ['items']
+    });
   }
 }
