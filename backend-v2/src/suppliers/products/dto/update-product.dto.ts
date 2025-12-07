f86e9c22a5e5c63188c498b42aaef2d51ac2c719
chinbino <shoptahrir40@gmail.com>
2025-12-07 15:06:10 +0330

    Update update-product.dto.ts

--- backend-v2/src/suppliers/products/dto/update-product.dto.ts
+++ backend-v2/src/suppliers/products/dto/update-product.dto.ts
@@ -1,4 +1,28 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProductDto } from './create-product.dto';
+import { IsOptional, IsString, IsNumber, IsEnum, IsInt } from 'class-validator';
+import { ProductStatus } from '../product.entity';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsNumber()
+  price?: number;
+
+  @IsOptional()
+  @IsInt()
+  stock?: number;
+
+  @IsOptional()
+  @IsInt()
+  min_order?: number;
+
+  @IsOptional()
+  @IsEnum(ProductStatus)
+  status?: ProductStatus;
+}
